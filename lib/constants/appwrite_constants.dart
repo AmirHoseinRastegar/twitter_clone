bf3c4a5b853cbfad74bade157c597e49bb39a9a1
amirpooyani19 <dota2ker@yahoo.com>
2024-10-29 17:06:45 +0330

    8 ABAN 1403 17:06 PM

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppWriteConstants {
+  static String databaseId = "67208fd300215e281d88";
+  static String projectId = "67208dbd00050ef07362";
+  static String endpoint = "http://localhost:80/v1";
+}
