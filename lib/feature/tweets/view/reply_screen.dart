0a7600e4783f8807d18586ef8056927f07a49c9a
amirpooyani19 <dota2ker@yahoo.com>
2024-11-26 16:31:31 +0330

    6 AZAR 1403 16:31 PM

--- lib/feature/tweets/view/reply_screen.dart
+++ lib/feature/tweets/view/reply_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/feature/tweets/controller/tweet_controller.dart';
 import 'package:twitter_clone/feature/tweets/widgets/tweets_card.dart';
 
+import '../../../common/error.dart';
+import '../../../constants/appwrite_constants.dart';
 import '../../../models/tweet_model.dart';
 
 class ReplyScreen extends ConsumerWidget {
@@ -22,14 +24,88 @@ class ReplyScreen extends ConsumerWidget {
           title: const Text('Tweet'),
           centerTitle: true,
         ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(15),
-            child: Column(
-              children: [
-                TweetsCard(tweetModel: tweetModel),
-              ],
-            ),
+        body: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            children: [
+              TweetsCard(tweetModel: tweetModel),
+              ref.watch(getRepliedTweetsProvider(tweetModel)).when(
+                  data: (tweets) {
+                    return ref.watch(getLatestTweetProvider).when(
+                          data: (data) {
+                            final latestTweets =
+                                TweetModel.fromJson(data.payload);
+                            bool isAlreadyTweeted = false;
+                            for (final tweetLoop in tweets) {
+                              if (tweetLoop.id == latestTweets.id) {
+                                isAlreadyTweeted = true;
+                                break;
+                              }
+                            }
+                            if (latestTweets.repliedTo == tweetModel.id &&
+                                !isAlreadyTweeted) {
+                              if (data.events.contains(
+                                  'databases.*.collections.${AppWriteConstants.tweetCollectionId}.documents.*.create')) {
+                                tweets.insert(
+                                    0, TweetModel.fromJson(data.payload));
+                              } else if (data.events.contains(
+                                  'databases.*.collections.${AppWriteConstants.tweetCollectionId}.documents.*.update')) {
+                                final startPoint =
+                                    data.events[0].lastIndexOf('documents.');
+                                final endPoint =
+                                    data.events[0].lastIndexOf('.update');
+                                final tweetId = data.events[0]
+                                    .substring(startPoint + 10, endPoint);
+                                var tweet = tweets
+                                    .where((element) => element.id == tweetId)
+                                    .first;
+                                final tweetIndex = tweets.indexOf(tweet);
+                                tweets.removeWhere(
+                                    (element) => element.id == tweetId);
+                                tweet = TweetModel.fromJson(data.payload);
+                                tweets.insert(tweetIndex, tweet);
+                              }
+                            }
+
+                            return Expanded(
+                              child: ListView.builder(
+                                padding: const EdgeInsets.only(
+                                  top: 10,
+                                ),
+                                itemCount: tweets.length,
+                                itemBuilder: (context, index) {
+                                  final tweetModel = tweets[index];
+                                  return TweetsCard(tweetModel: tweetModel);
+                                },
+                              ),
+                            );
+                          },
+                          error: (error, stackTrace) {
+                            return ErrorMessage(
+                              errorMessage: error.toString(),
+                            );
+                          },
+                          loading: () => Expanded(
+                            child: ListView.builder(
+                              padding: const EdgeInsets.only(
+                                top: 10,
+                              ),
+                              itemCount: tweets.length,
+                              itemBuilder: (context, index) {
+                                final tweetModel = tweets[index];
+                                return TweetsCard(tweetModel: tweetModel);
+                              },
+                            ),
+                          ),
+                        );
+                  },
+                  error: (error, stackTrace) {
+                    return ErrorMessage(
+                      errorMessage: error.toString(),
+                    );
+                  },
+                  loading: () => Container())
+            ],
           ),
         ),
         bottomNavigationBar: TextField(
